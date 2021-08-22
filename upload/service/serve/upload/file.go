50f2accdbf14785f91f1eee33dbad31ca140a9d9
leetaifook <lee@leetaifook.com>
2021-08-22 18:45:59 +0800

    add delete file

--- upload/service/serve/upload/file.go
+++ upload/service/serve/upload/file.go
@@ -2,6 +2,7 @@ package upload
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/haiyiyun/log"
 	"github.com/haiyiyun/plugins/upload/database/model"
@@ -9,6 +10,7 @@ import (
 	"github.com/haiyiyun/plugins/upload/predefined"
 	"github.com/haiyiyun/plugins/upload/service/local"
 	"github.com/haiyiyun/utils/help"
+	"github.com/haiyiyun/utils/http/request"
 	"github.com/haiyiyun/utils/http/response"
 	"github.com/haiyiyun/validator"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -112,4 +114,62 @@ func (self *Service) Route_POST_File(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	response.JSON(rw, http.StatusBadRequest, nil, "上传失败")
+}
+
+func (self *Service) Route_DELETE_File(rw http.ResponseWriter, r *http.Request) {
+	userID := primitive.NilObjectID
+
+	if self.Config.CheckUser {
+		if cliaims := request.GetClaims(r); cliaims != nil {
+			if uid, err := primitive.ObjectIDFromHex(cliaims.Issuer); err != nil {
+				response.JSON(rw, http.StatusUnauthorized, nil, "")
+				return
+			} else {
+				userID = uid
+			}
+		} else {
+			response.JSON(rw, http.StatusUnauthorized, nil, "")
+			return
+		}
+	}
+
+	fileIDStr := r.URL.Query().Get("id")
+	valid := &validator.Validation{}
+	valid.Required(fileIDStr).Key("id").Message("id字段为空")
+
+	if valid.HasErrors() {
+		response.JSON(rw, http.StatusBadRequest, nil, valid.RandomError().String())
+		return
+	}
+
+	fileID, fileIDErr := primitive.ObjectIDFromHex(fileIDStr)
+	if fileIDErr != nil {
+		response.JSON(rw, http.StatusBadRequest, nil, "")
+		return
+	}
+
+	uploadModel := upload.NewModel(self.M)
+	filter := uploadModel.FilterByID(fileID)
+	filter = append(filter, uploadModel.FilterByUserID(userID)...)
+
+	sr := uploadModel.FindOne(r.Context(), uploadModel.FilterByID(fileID))
+	var uploadFile model.Upload
+	if err := sr.Decode(&uploadFile); err != nil {
+		response.JSON(rw, http.StatusNotFound, nil, "")
+	} else {
+		if uploadFile.Storage == predefined.UploadStorageLocal {
+			filePath := self.Config.UploadDirectory + uploadFile.Path
+			if err := os.Remove(filePath); err != nil {
+				response.JSON(rw, http.StatusNotFound, nil, "")
+			} else {
+				if dr, err := uploadModel.DeleteOne(r.Context(), filter); err != nil || dr.DeletedCount == 0 {
+					response.JSON(rw, http.StatusNotFound, nil, "")
+				} else {
+					response.JSON(rw, http.StatusOK, nil, "")
+				}
+			}
+		} else {
+			response.JSON(rw, http.StatusServiceUnavailable, nil, "")
+		}
+	}
 }
