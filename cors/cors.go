79a22dde005446710d3e8012bc1fec2708055818
leetaifook <lee@leetaifook.com>
2021-11-05 07:11:38 +0800

    fix header write

--- cors/cors.go
+++ cors/cors.go
@@ -8,15 +8,14 @@ import (
 
 func init() {
 	webrouter.Injector("cors", "", 999, func(rw http.ResponseWriter, r *http.Request) (abort bool) {
-		method := r.Method
-		origin := r.Header.Get("Origin")
-		rw.Header().Set("Access-Control-Allow-Origin", origin)
-		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
-		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
-		rw.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		rw.Header().Set("Access-Control-Allow-Credentials", "true")
-		if method == "OPTIONS" {
+		if r.Method == "OPTIONS" {
 			abort = true
+			origin := r.Header.Get("Origin")
+			rw.Header().Set("Access-Control-Allow-Origin", origin)
+			rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, AccessToken, X-CSRF-Token, Authorization, Token, X-Token, X-User-Id")
+			rw.Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST, GET, DELETE, PUT")
+			rw.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+			rw.Header().Set("Access-Control-Allow-Credentials", "true")
 			rw.WriteHeader(http.StatusNoContent)
 		}
 
